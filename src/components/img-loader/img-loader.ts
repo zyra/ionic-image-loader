0cc2c225728094cfc2eea9893c0d7bb1c22e2977
Ibby <ibby93@gmail.com>
2017-02-17 18:05:09 -0500

    feat(img-loader): ability to disable caching and update images

--- src/components/img-loader/img-loader.ts
+++ src/components/img-loader/img-loader.ts
@@ -11,7 +11,17 @@ export class ImgLoader implements OnInit {
   /**
    * The URL of the image to load.
    */
-  @Input('src') imageUrl: string;
+  @Input()
+  set src(imageUrl: string) {
+    this._src = this.processImageUrl(imageUrl);
+    this.updateImage(this._src);
+  };
+
+  get src(): string {
+    return this._src;
+  }
+
+  private _src: string;
 
   /**
    * Fallback URL to load when the image url fails to load or does not exist.
@@ -32,6 +42,21 @@ export class ImgLoader implements OnInit {
   }
 
   private _useImg: boolean = this.config.useImg;
+
+  /**
+   * Convenience attribute to disable caching
+   * @param val
+   */
+  @Input()
+  set noCache(val: boolean) {
+    this.cache = val !== false;
+  }
+
+  /**
+   * Enable/Disable caching
+   * @type {boolean}
+   */
+  @Input() cache: boolean = true;
 
   /**
    * Width of the image. This will be ignored if using useImg.
@@ -64,6 +89,8 @@ export class ImgLoader implements OnInit {
    */
   isLoading: boolean = true;
 
+  private imgElement: HTMLImageElement;
+
   constructor(
     private element: ElementRef
     , private renderer: Renderer
@@ -72,7 +99,7 @@ export class ImgLoader implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    if (!this.imageUrl) {
+    if (!this.src) {
       // image url was not passed
       // this can happen when [src] is set to a variable that turned out to be undefined
       // one example could be a list of users with their profile pictures
@@ -80,34 +107,64 @@ export class ImgLoader implements OnInit {
       if (this.fallbackUrl) {
         // we're not going to cache the fallback image since it should be locally saved
         this.setImage(this.fallbackUrl);
+      } else {
+        this.isLoading = false;
       }
-      // remove the spinner just in case there is no fallback image
-      this.isLoading = false;
-      return;
     }
-
-    this.imageLoader.getImagePath(this.imageUrl)
-      .then((imageUrl: string) => this.setImage(imageUrl))
-      .catch((error: any) => this.setImage(this.fallbackUrl || this.imageUrl));
   }
 
-  private setImage(imageUrl: string): void {
-    let element;
+  private updateImage(imageUrl: string) {
+    this.imageLoader.getImagePath(imageUrl)
+      .then((imageUrl: string) => this.setImage(imageUrl))
+      .catch((error: any) => this.setImage(this.fallbackUrl || imageUrl));
+  }
 
+  /**
+   * Gets the image URL to be loaded and disables caching if necessary
+   * @returns {string}
+   */
+  private processImageUrl(imageUrl: string): string {
+    if (this.cache === false) {
+      // need to disable caching
+
+      if (imageUrl.indexOf('?') === -1) { // add ? if doesn't exists
+        imageUrl += '?';
+      }
+
+      if (['&', '?'].indexOf(imageUrl.charAt(imageUrl.length)) === -1) { // add & if necessary
+        imageUrl += '&';
+      }
+
+      // append timestamp at the end to make URL unique
+      imageUrl += 'cache_buster=' + Date.now();
+    }
+
+    return imageUrl;
+  }
+
+  /**
+   * Set the image to be displayed
+   * @param imageUrl
+   */
+  private setImage(imageUrl: string): void {
     this.isLoading = false;
 
     if (this._useImg) {
 
       // Using <img> tag
-      this.renderer.createElement(this.element.nativeElement, 'img');
-      element = <HTMLImageElement>this.element.nativeElement.getElementsByTagName('IMG')[0];
-      this.renderer.setElementAttribute(element, 'src', imageUrl);
+      if (!this.imgElement) {
+        // create img element if we dont have one
+        this.imgElement = this.renderer.createElement(this.element.nativeElement, 'img');
+      }
+
+      // set it's src
+      this.renderer.setElementAttribute(this.imgElement, 'src', imageUrl);
 
     } else {
 
       // Not using <img> tag
 
-      element = this.element.nativeElement;
+      const element = this.element.nativeElement;
 
       if (this.display) {
         this.renderer.setElementStyle(element, 'display', this.display);
