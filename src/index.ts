d64588f42d4b83c55612dca11956134b631901c4
Ibby <ibby93@gmail.com>
2017-03-21 22:06:08 -0400

    remove reference to spinner component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './image-loader.module';
 export * from './components/img-loader';
-export * from './components/image-loader-spinner/image-loader-spinner';
 export * from './providers/image-loader-config';
 export * from './providers/image-loader';
