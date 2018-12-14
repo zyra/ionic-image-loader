3050d1787f492d81fb085280f871c63a1b21d5df
Ibby Hadeed <ibby@zyra.ca>
2018-12-14 04:57:26 -0500

    Migrate config from previous proj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { Platform } from '@ionic/angular';
+import { ImageLoaderConfigService } from '../../projects/ionic-image-loader/src/lib/services/image-loader-config.service';
 
 
 @Component({
@@ -14,8 +15,23 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    imageLoaderConfig: ImageLoaderConfigService,
   ) {
     this.initializeApp();
+
+    // enable debug mode to get console logs and stuff
+    imageLoaderConfig.enableDebugMode();
+    // set a fallback url to use by default in case an image is not found
+    imageLoaderConfig.setFallbackUrl('assets/fallback.png');
+
+    imageLoaderConfig.setImageReturnType('base64');
+
+    imageLoaderConfig.setSpinnerColor('secondary');
+    imageLoaderConfig.setSpinnerName('bubbles');
+
+
+    imageLoaderConfig.maxCacheSize = 2 * 1024 * 1024; // 2 MB
+    imageLoaderConfig.maxCacheAge = 60 * 1000; // 1 minute
   }
 
   initializeApp() {
