699446761f32f7e0ce6b384d87046877ac196dfd
Ibby Hadeed <ibby@zyra.ca>
2018-12-21 08:59:58 -0500

    fix(): export services

--- projects/ionic-image-loader/src/public_api.ts
+++ projects/ionic-image-loader/src/public_api.ts
@@ -3,3 +3,5 @@
  */
 export * from './lib/ionic-image-loader.component';
 export * from './lib/ionic-image-loader.module';
+export * from './lib/services/image-loader.service';
+export * from './lib/services/image-loader-config.service';
